727aff27dcd5bab4f3859e65f3d642086b4df2e8
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-05 09:02:18 +0000

    update seeds & posts page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,17 @@
   )
 end
 
-# 3.times do
-#   post = Post.create(
-#     title: Faker::Lorem.sentence,
-#     body: Faker::Lorem.paragraph
-#   )
-#   3.times do
-#     comment = post.comments.build(
-#       body: Faker::Lorem.paragraph
-#     )
-#     comment.save
-#   end
-# end
+1.times do
+  user = User.create(
+    email: "test@test.com",
+    password: "testtest123"
+  )
+  3.times do
+    post = user.posts.build(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph,
+      post_category_id: rand(0..4)
+    )
+    post.save
+  end
+end
