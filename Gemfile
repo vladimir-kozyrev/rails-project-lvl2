38a5b38c133e163d33fb9b62f52c90162d42044a
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-09 08:55:15 +0000

    add rollbar

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'devise'
 gem 'slim-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'rollbar'
 gem 'rubocop-discourse'
 gem 'simple_form'
 gem 'slim_lint', '~> 0.22.1'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
-# Load the Rails application.
-require_relative 'application'
+# https://docs.rollbar.com/docs/rails
+# rubocop:disable Style/ExpandPathArguments
+# rubocop:disable Style/Lambda
+# rubocop:disable Style/RescueStandardError
+# rubocop:disable Lint/RescueException
+# rubocop:disable Style/LambdaCall
+require File.expand_path('../application', __FILE__)
+require File.expand_path('../rollbar', __FILE__)
 
-# Initialize the Rails application.
-Rails.application.initialize!
+notify = ->(e) do
+  begin
+    Rollbar.with_config(use_async: false) do
+      Rollbar.error(e)
+    end
+  rescue
+    Rails.logger.error 'Synchronous Rollbar notification failed. Sending async to preserve info.'
+    Rollbar.error(e)
+  end
+end
+
+begin
+  Rails.application.initialize!
+rescue Exception => e
+  notify.(e)
+  raise
+end
+# rubocop:enable Style/ExpandPathArguments
+# rubocop:enable Style/Lambda
+# rubocop:enable Style/RescueStandardError
+# rubocop:enable Lint/RescueException
+# rubocop:enable Style/LambdaCall
