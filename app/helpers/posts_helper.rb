165cd513151c3ba287cc26d91372394761414995
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-09 12:52:17 +0000

    make post#index look more like the example

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  def days_ago_since(created_at)
+    time_now = Time.now
+    creaton_on_date = Date.parse("#{created_at.day}/#{created_at.month}/#{created_at.year}")
+    current_date = Date.parse("#{time_now.day}/#{time_now.month}/#{time_now.year}")
+    current_date.mjd - creaton_on_date.mjd
+  end
 end
