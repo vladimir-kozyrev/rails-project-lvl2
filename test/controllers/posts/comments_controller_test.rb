18c53048299824256131fc6a47d6d4401bf906b0
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-12 16:37:55 +0000

    fix rubocop issues

--- test/controllers/posts/comments_controller_test.rb
+++ test/controllers/posts/comments_controller_test.rb
@@ -2,32 +2,34 @@
 
 require 'test_helper'
 
-class Posts::CommentsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
+module Posts
+  class CommentsControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
 
-  setup do
-    @comment = post_comments(:one)
-    # sign in to be able to do test actions
-    get '/users/sign_in'
-    sign_in users(:one)
-    post user_session_url
-    follow_redirect!
-    assert_response :success
-  end
-
-  test 'should create comment' do
-    assert_difference('PostComment.count') do
-      post post_comments_path(@comment.post), params: {
-        post_comment: {
-          content: @comment.content,
-          post_id: @comment.post_id
-        }
-      }
+    setup do
+      @comment = post_comments(:one)
+      # sign in to be able to do test actions
+      get '/users/sign_in'
+      sign_in users(:one)
+      post user_session_url
+      follow_redirect!
+      assert_response :success
     end
-  end
 
-  test 'should get new' do
-    get new_post_comment_path(@comment.post)
-    assert_response :success
+    test 'should create comment' do
+      assert_difference('PostComment.count') do
+        post post_comments_path(@comment.post), params: {
+          post_comment: {
+            content: @comment.content,
+            post_id: @comment.post_id
+          }
+        }
+      end
+    end
+
+    test 'should get new' do
+      get new_post_comment_path(@comment.post)
+      assert_response :success
+    end
   end
 end
--- test/controllers/posts/likes_controller_test.rb
+++ test/controllers/posts/likes_controller_test.rb
@@ -1,39 +1,43 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class Posts::LikesControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
+require 'test_helper'
 
-  setup do
-    @like = post_likes(:one)
-    @user = users(:one)
-    @post = posts(:two)
-    # sign in to be able to do test actions
-    get '/users/sign_in'
-    sign_in users(:one)
-    post user_session_url
-    follow_redirect!
-    assert_response :success
-  end
+module Posts
+  class LikesControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
 
-  test 'should like post if it isn\'t already liked' do
-    assert_difference('PostLike.count', +1) do
-      post post_likes_path(@post.id), params: {
-        post_like: {
-          user_id: @user.id,
-          post_id: @post.id
-        }
-      }
+    setup do
+      @like = post_likes(:one)
+      @user = users(:one)
+      @post = posts(:two)
+      # sign in to be able to do test actions
+      get '/users/sign_in'
+      sign_in users(:one)
+      post user_session_url
+      follow_redirect!
+      assert_response :success
     end
-  end
 
-  test 'should remove like if already liked' do
-    assert_difference('PostLike.count', -1) do
-      post post_likes_path(@like.post), params: {
-        post_like: {
-          user_id: @like.user_id,
-          post_id: @like.post_id
+    test 'should like post if it isn\'t already liked' do
+      assert_difference('PostLike.count', +1) do
+        post post_likes_path(@post.id), params: {
+          post_like: {
+            user_id: @user.id,
+            post_id: @post.id
+          }
         }
-      }
+      end
+    end
+
+    test 'should remove like if already liked' do
+      assert_difference('PostLike.count', -1) do
+        post post_likes_path(@like.post), params: {
+          post_like: {
+            user_id: @like.user_id,
+            post_id: @like.post_id
+          }
+        }
+      end
     end
   end
 end
