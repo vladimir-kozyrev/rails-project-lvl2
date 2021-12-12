05d382e1305529156f2f40044604d428ec494d63
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-12 10:15:15 +0000

    show nested comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module CommentsHelper
+  def nested_comments(comments)
+    return if comments.nil?
+
+    comments.map do |comment|
+      render(partial: 'posts/comments/comment', locals: { comment: comment }) +
+        content_tag(:div, nested_comments(comment.children), class: 'nested_comments')
+    end.join.html_safe
+  end
 end
