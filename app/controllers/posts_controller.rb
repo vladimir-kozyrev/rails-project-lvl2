7a5dad8f44dd123dd19746fd0a31724920682e29
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-24 06:48:09 +0000

    render without status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
