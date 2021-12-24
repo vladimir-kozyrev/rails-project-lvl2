1374b8362959c1290704ba24f61f3387fee155bf
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-12-24 06:54:22 +0000

    hexlet tests expect a redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
-      render :new
+      redirect_to new_post_path
     end
   end
 
